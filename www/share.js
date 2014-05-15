ad8d057a56d29fb96548ece4749bbf0a63476a8b
shikatah <shikatah@asial.co.jp>
2014-05-15 18:21:34 +0900

    add js file.¬

---
+++ www/share.js
@@ -0,0 +1,12 @@
+
+var Share = function() {};
+            
+Share.prototype.show = function(content, success, fail) {
+    return cordova.exec( function(args) {
+        success(args);
+    }, function(args) {
+        fail(args);
+    }, 'Share', '', [content]);
+};
+    
+module.exports = new Share();
