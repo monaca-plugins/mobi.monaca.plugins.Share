3771d8874f9fd74f11c30b80a228c6927a1cde8b
Sotheara <sotheara@asial.co.jp>
2014-05-28 18:47:59 +0900

    fix plugin

--- src/android/Share.java
+++ src/android/Share.java
@@ -1,27 +1,43 @@
 
 package mobi.monaca.framework.plugin;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Intent;
 
-import org.apache.cordova.api.Plugin;
-import org.apache.cordova.api.PluginResult;
+public class Share extends CordovaPlugin {
+    
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
 
-public class Share extends Plugin {
-
-	@Override
-	public PluginResult execute(String action, JSONArray args, String callbackId) {
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action            The action to execute.
+     * @param args              JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return                  True if the action was valid, false if not.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        
 		try {
 			JSONObject jo = args.getJSONObject(0);
 			doSendIntent(jo.getString("subject"), jo.getString("text"));
-			return new PluginResult(PluginResult.Status.OK);
+			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
 		} catch (JSONException e) {
-			return new PluginResult(PluginResult.Status.JSON_EXCEPTION);
+			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.JSON_EXCEPTION));
 		}
-	}
+		
+        return true;
+    }
 
 	private void doSendIntent(String subject, String text) {
 		Intent sendIntent = new Intent(android.content.Intent.ACTION_SEND);
